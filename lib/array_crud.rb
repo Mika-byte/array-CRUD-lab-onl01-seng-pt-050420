cfcb0a7f19be7f29888989bceee3210e210f4a88
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-13 02:27:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,9 +8,9 @@ def create_an_array
   
 end
 
-def add_element_to_end_of_array(array, element)
-  array
-  
+def add_element_to_end_of_array
+  ["a", "b", "c"]
+  <<"d"
 end
 
 def add_element_to_start_of_array(array, element)
